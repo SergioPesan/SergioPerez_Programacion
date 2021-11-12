f45960da42f2fa4d0564ec7e31f46367d686773b
SergioPesan <sergio.perez@cesjuanpablosegundo.es>
2021-11-12 11:55:26 +0100

    Arrays

--- Tema 4/T4_Arrays/src/Arrays.java
+++ Tema 4/T4_Arrays/src/Arrays.java
@@ -1,6 +1,6 @@
 public class Arrays {
     public static void main(String[] args) {
-
+        /*
         int[] numerosUno = new int[10]; // numerosUno = [0,0,0,0,0,0,0,0,0,0]
         int[] numerosDos = new int[] {1,2,3,4,5,6,7,8,9,10};
 
@@ -17,11 +17,27 @@ public class Arrays {
         for (int i = 0; i <=  numerosDos.length -1; i++) {
             System.out.println(numerosDos[i]);
         }
+        numerosDos[7] = 88;
+
+        System.out.println("Modifico alguna posición");
+        for (int i = 0; i <=  numerosDos.length -1; i++) {
+            System.out.println(numerosDos[i]);
+        }*/
+
+        //ejercicio 1
+
+        int[] posicion = new int[15];
+        int par=0, impar=0;
+
+        for (int i = 0; i < posicion.length; i++) {
+            int numAleatorio =(int) (Math.random()*50);
+            System.out.println(numAleatorio);
+        }
     }
 }
 /* De los arrays nos puede interesar:
     Saber cuantas posiciones hay -> .length
-    Saber el valor de una posición -> sout-variable[posición]
-    Cómo doy valor a posiciones
-    Cómo saco todos los valores
- */
+    Saber el valor de una posición -> sout-variable[posición]*/
+
+//meter en un array de 15 posiciones, 15 numeros aleatorios entre 0 - 50
+//una vez generados los numeros, indicar cuantos pares e impares hay en la coleccion
