f83251179c22f2120364a527277b2fa0c0bc748a
SergioPesan <sergio.perez@cesjuanpablosegundo.es>
2022-03-21 14:32:12 +0100

    Partido

--- Tema7/T07_ejPartido/src/Entrada.java
+++ Tema7/T07_ejPartido/src/Entrada.java
@@ -1,8 +1,43 @@
 public class Entrada {
+    public static void main(String[] args) {
+
     Partido partido = new Partido();
 
-    Partido.Equipo equipo1 = partido.new Equipo(3,"Barcelona");
-    Partido.Equipo equipo2 = partido.new Equipo(0, "Real Madrid");
+    Partido.Equipo atleti = partido.new Equipo("Atleti");
+    Partido.Equipo sevilla = partido.new Equipo("Sevilla");
+
+    partido.mostrarResultado();
+
+        System.out.println(" ");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+            partido.agregarGolesLocal();
+            partido.agregarGolesVisitante();
+            }
+            System.out.println("El resultado en la " + (i+1)+ "ª" + " parte es: ");
+            partido.mostrarResultado();
+        }
+
+        partido.repartirPuntos();
+        partido.escribirClasificacion();
+        partido.reiniciarPartido();
+
+        System.out.println(" ");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                partido.agregarGolesLocal();
+                partido.agregarGolesVisitante();
+            }
+            System.out.println("El resultado en la " + (i+1)+ "ª" + " parte es: ");
+            partido.mostrarResultado();
+        }
+
+        partido.repartirPuntos();
+        partido.escribirClasificacion();
 
 
+
+    }
 }
--- Tema7/T07_ejPartido/src/Partido.java
+++ Tema7/T07_ejPartido/src/Partido.java
@@ -1,22 +1,51 @@
-public class Partido {
+public final class Partido {
 
     //atributos
-    private Equipo equipoLocal, EquipoVisitante;
+    private Equipo equipoLocal, equipoVisitante;
     private boolean jugado;
     private int golesLocal, golesVisitante;
 
     //Métodos
     public void agregarGolesLocal(){
-        golesLocal += (int)(Math.random()*2);
+        golesLocal += (int)(Math.random()*3);
     }
 
     public void agregarGolesVisitante(){
-        golesVisitante += (int)(Math.random()*2);
+        golesVisitante += (int)(Math.random()*3);
     }
 
     public void mostrarResultado(){
         System.out.println(equipoLocal.getNombre() + " " + golesLocal + " - " +
                 golesVisitante + " " + getEquipoVisitante().getNombre());
+
+        /*System.out.printf("%s: %d - %s: %d",equipoLocal.getNombre(),
+                golesLocal, golesVisitante, equipoVisitante.getNombre());*/
+    }
+
+    public void repartirPuntos(){
+        if (golesVisitante > golesLocal){
+            this.equipoVisitante.setPuntos(equipoVisitante.getPuntos()+3);
+        }else if (golesLocal > golesVisitante){
+            this.equipoLocal.setPuntos(equipoLocal.getPuntos()+3);
+        }else if (golesLocal == golesVisitante){
+            this.equipoVisitante.setPuntos(equipoVisitante.getPuntos()+1);
+            this.equipoLocal.setPuntos(equipoLocal.getPuntos()+1);
+        }
+    }
+
+    public void escribirClasificacion(){
+        if (equipoLocal.getPuntos()>equipoVisitante.getPuntos()){
+            System.out.println("1- Atleti: "+equipoLocal.getPuntos());
+            System.out.println("2- Sevilla: "+equipoVisitante.getPuntos());
+        }else if (equipoLocal.getPuntos()<equipoVisitante.getPuntos()){
+            System.out.println("1- Sevilla: "+equipoVisitante.getPuntos());
+            System.out.println("2- Atleti: "+equipoLocal.getPuntos());
+        }
+    }
+
+    public void reiniciarPartido(){
+        setGolesLocal(0);
+        setGolesVisitante(0);
     }
 
     //Getter y Setter
@@ -29,11 +58,11 @@ public class Partido {
     }
 
     public Equipo getEquipoVisitante() {
-        return EquipoVisitante;
+        return equipoVisitante;
     }
 
     public void setEquipoVisitante(Equipo equipoVisitante) {
-        EquipoVisitante = equipoVisitante;
+        this.equipoVisitante = equipoVisitante;
     }
 
     public boolean isJugado() {
@@ -69,11 +98,15 @@ public class Partido {
 
         //Constructores
         public Equipo(){}
-        public Equipo(int puntos, String nombre) {
-            this.puntos = puntos;
+        public Equipo(String nombre) {
             this.nombre = nombre;
-            setEquipoLocal(this);
-            setEquipoVisitante(this);
+            if (nombre.equalsIgnoreCase("Atleti")){
+                setEquipoLocal(this);
+            }else if (nombre.equalsIgnoreCase("Sevilla")){
+                setEquipoVisitante(this);
+            }
+
+
         }
 
         //Getter y Setter
